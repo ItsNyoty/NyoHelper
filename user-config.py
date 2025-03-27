bcd5adafbd3e50c4a1e1530dfc69a86b10c7debc
Nyo <58821897+ItsNyoty@users.noreply.github.com>
2025-03-27 17:34:27 +0100

    Create user-config.py

---
+++ user-config.py
@@ -0,0 +1,9 @@
+mylang = 'nl'
+family = 'wikipedia'
+usernames['wikipedia']['nl'] = "Nyo's Helper"
+
+passwords = {
+    "wikipedia": {
+        "nl": input("Voer het botwachtwoord in voor de Nederlandstalige Wikipedia: ")
+    }
+}
